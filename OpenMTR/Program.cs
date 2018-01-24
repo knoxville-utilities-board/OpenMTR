667df9e2c811c05fb09a06631ebfff06cb1f0058
Bryan Dingman <bsdingman@gmail.com>
2018-01-24 14:51:53 -0500

    feat: Added calls to method in Welcome Class

--- OpenMTR/Program.cs
+++ OpenMTR/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,23 @@ using OpenCvSharp;
 
 namespace OpenMTR
 {
-    class Program
+    partial class Program
     {
+        // The main directory path that contains all the photos and metadata
+        public static string DirectoryPath { get; set; }
+
+        // Set this to false if you don't want debug logs. 
+        public const bool DEBUG = true;
+
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            // Welcomes the user ane gets the directory path to process
+            Welcome.WelcomeUser();
+            Console.Read();
         }
     }
 }
