041c1cee8f9ced1acf358a2a8b63f8193dfc47a1
Bryan Dingman <bsdingman@gmail.com>
2018-01-31 11:18:50 -0500

    Chore: Add DataObject class

---
+++ OpenMTR/code/DataObject.cs
@@ -0,0 +1,11 @@
+﻿using OpenCvSharp;
+
+namespace OpenMTR.code
+{
+    public class DataObject
+    {
+        public string FileName { get; set; }
+        public Mat Image { get; set; }
+        public int MeterRead { get; set; }
+    }
+}
