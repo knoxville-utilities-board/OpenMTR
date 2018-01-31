46e9c568a284439dac15cf2353c17956a376aca4
Bryan Dingman <bsdingman@gmail.com>
2018-01-31 11:34:27 -0500

    Changed: Added constructors

--- OpenMTR/code/DataObject.cs
+++ OpenMTR/code/DataObject.cs
@@ -7,5 +7,13 @@ namespace OpenMTR
         public string FileName { get; set; }
         public Mat Image { get; set; }
         public int MeterRead { get; set; }
+
+        public DataObject() { }
+        public DataObject(string name, Mat imageMat, int parsedMeterMetaData)
+        {
+            this.FileName = name;
+            this.Image = imageMat;
+            this.MeterRead = parsedMeterMetaData;
+        }
     }
 }
