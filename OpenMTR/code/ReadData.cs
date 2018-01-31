4f9e21b8991931760850fcb08e71f510d062c9a3
xervir <nchietala@gmail.com>
2018-01-31 16:26:27 -0500

    Nick iterator (#17)
    * Chore: created FileHandles class
    
    * Refactor: Rebuilt filehandler class to work with and use the DataObject class
    
    * Fix: set line endings
    
    * test: checking line endings problem
    
    * refactor: modified ReadData class
    
    * fix: adjusted try-catch block
    
    * fix: shortened ReadMetaData method
    
    * fix: adjusted ReadMetaData method
    
    * fix: shortened ReadMetaData method

---
+++ OpenMTR/code/ReadData.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System;
+using System.Text.RegularExpressions;
+using OpenCvSharp;
+using System.Collections.Generic;
+
+namespace OpenMTR
+{
+    public static class ReadData
+    {
+        // Get a list of data objects built from all the images found in a directory
+        public static List<DataObject> GetDataObjectList(string directoryPath)
+        {
+            List<DataObject> data = new List<DataObject>();
+            foreach (string filePath in Directory.GetFiles(directoryPath))
+            {
+                if (Regex.IsMatch(filePath, @"\.(jpe?g|png)$", RegexOptions.IgnoreCase))
+                {
+                    data.Add(GetDataObject(filePath));
+                }
+            }
+            return data;
+        }
+        public static void GetDataObjectList(string directoryPath, List<DataObject> data)
+        {
+            data = GetDataObjectList(directoryPath);
+        }
+
+        // Get a single data object from a path to the image file
+        private static DataObject GetDataObject(string imagePath)
+        {
+            string Filename = Path.GetFileNameWithoutExtension(imagePath);
+            return new DataObject(Filename, new Mat(imagePath), ReadMetaData(imagePath));
+        }
+
+        // Get a metadata file path with the same name as the provided file
+        private static int ReadMetaData(string imagePath)
+        {
+            string metaPath = Regex.Replace(Path.GetFullPath(imagePath), @"\.(jpe?g|png)$", ".txt", RegexOptions.IgnoreCase);
+            try
+            {
+                return int.Parse(File.ReadAllText(metaPath));
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
