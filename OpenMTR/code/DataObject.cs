3730f17be57ccb4b5e6f2b1c1796349a041b088d
Matt <cclinard84@gmail.com>
2018-01-31 19:55:27 -0500

    fix: Changed setter accessors to allow setting outside of class (#18)

--- OpenMTR/code/DataObject.cs
+++ OpenMTR/code/DataObject.cs
@@ -4,9 +4,9 @@ namespace OpenMTR
 {
     public class DataObject
     {
-        public string FileName { get; private set; }
-        public Mat Image { get; private set; }
-        public int MeterRead { get; private set; }
+        public string FileName { get; set; }
+        public Mat Image { get; set; }
+        public int MeterRead { get; set; }
 
         public DataObject() { }
         public DataObject(string fileName, Mat image, int meterRead)
