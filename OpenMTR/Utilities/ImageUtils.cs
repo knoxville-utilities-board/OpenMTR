1ebec01f356b3ecd41d3f17d0611dc75c9ced988
Matt Clinard <cclinard84@gmail.com>
2018-01-31 20:25:00 -0500

    chore: Created Utilites folder and created ImageUtils class with methods

---
+++ OpenMTR/Utilities/ImageUtils.cs
@@ -0,0 +1,19 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR.code
+{
+    public static class ImageUtils
+    {
+        public static void ColorToGray(DataObject dataObject)
+        {
+            Mat tempImage = new Mat();
+            Cv2.CvtColor(dataObject.Image, tempImage, ColorConversionCodes.BGR2GRAY);
+            dataObject.Image = tempImage;
+        }
+    }
+}
