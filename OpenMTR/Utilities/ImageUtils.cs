cc1b48439340185c66eee417669a77dd1d163260
Matt <cclinard84@gmail.com>
2018-02-01 23:17:16 -0500

    feat: Created ImageUtils class and moved all Utility classes to Utility Folder (#19)
    * chore: Created Utilites folder and created ImageUtils class with methods
    
    * feat: Added ApplyGuassianBlur method
    
    * feat: Added overloaded ColorToGray method to allow for List of dataObjects
    
    * feat: Added overloaded ApplyGuassianBlur method to accept list of dataObjects
    
    * fix: Rebuilt .csproj to reflect movement of debugUtils class
    
    * fix: Fixed namespace per review

---
+++ OpenMTR/Utilities/ImageUtils.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR
+{
+    public static class ImageUtils
+    {
+        public static void ColorToGray(DataObject dataObject)
+        {
+            Cv2.CvtColor(dataObject.Image, dataObject.Image, ColorConversionCodes.BGR2GRAY);
+        }
+
+        public static void ColorToGray(List<DataObject> dataObjectList)
+        {
+            foreach (DataObject dataObject in dataObjectList)
+            {
+                Cv2.CvtColor(dataObject.Image, dataObject.Image, ColorConversionCodes.BGR2GRAY);
+            }
+        }
+
+        public static void ApplyGaussianBlur(DataObject dataObject)
+        {
+            Cv2.GaussianBlur(dataObject.Image, dataObject.Image, new Size(3, 3), 0, 0, BorderTypes.Default);
+        }
+
+        public static void ApplyGaussianBlur(List<DataObject> dataObjectList)
+        {
+            foreach(DataObject dataObject in dataObjectList)
+            {
+                Cv2.GaussianBlur(dataObject.Image, dataObject.Image, new Size(3, 3), 0, 0, BorderTypes.Default);
+            }
+        }
+    }
+}
