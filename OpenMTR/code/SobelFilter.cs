00c1279ef5aa8c1eaffe0ad9a8022d62cadebffc
Matt Clinard <cclinard84@gmail.com>
2018-02-02 17:52:37 -0500

    feat: Created sobel filter method

---
+++ OpenMTR/code/SobelFilter.cs
@@ -0,0 +1,18 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR.code
+{
+    public static class SobelFilter
+    {
+        public static void ApplySobelFilter(DataObject dataObject)
+        {
+            Mat tempImage = new Mat();
+            Cv2.Sobel(tempImage, dataObject.Image, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
+        }
+    }
+}
