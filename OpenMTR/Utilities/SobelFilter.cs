6fc07ad6e83e706c4e3c20e111d0a168fe8c5927
Matt <cclinard84@gmail.com>
2018-02-02 22:44:09 -0500

    feat: Added Sobel Filtering class (#21)
    * feat: Created sobel filter method
    
    * fix: Using modifiedImage instead of image from DataObject class
    
    * fix: Modified method parameters

---
+++ OpenMTR/Utilities/SobelFilter.cs
@@ -0,0 +1,25 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR
+{
+    public static class SobelFilter
+    {
+        public static void ApplySobelFilter(Mat source, Mat destination)
+        {
+            Cv2.Sobel(source, destination, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
+        }
+
+        public static void ApplySobelFilter(List<DataObject> dataObjectList)
+        {
+            foreach(DataObject dataObject in dataObjectList)
+            {
+                Cv2.Sobel(dataObject.ModifiedImage, dataObject.ModifiedImage, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
+            }
+        }
+    }
+}
