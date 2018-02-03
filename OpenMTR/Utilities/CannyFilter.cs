93c09a13899620c11fee37664cd652a6b20169b4
Bryan Dingman <bsdingman@gmail.com>
2018-02-02 23:06:57 -0500

    feat: Add canny filter.

---
+++ OpenMTR/Utilities/CannyFilter.cs
@@ -0,0 +1,30 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR
+{
+    public static class CannyFilter
+    {
+        public static void ApplyCannyFilter(Mat sourceImage, Mat destinationImage)
+        {
+            Cv2.Canny(sourceImage, destinationImage, 100, 150);
+        }
+
+        public static void ApplyCannyFilter(Mat sourceImage, Mat destinationImage, double minThreshold, double maxThreshold)
+        {
+            Cv2.Canny(sourceImage, destinationImage, minThreshold, maxThreshold);
+        }
+
+        public static void ApplyCannyFilter(List<DataObject> dataObjectList)
+        {
+            foreach (DataObject dataObject in dataObjectList)
+            {
+                Cv2.Canny(dataObject.ModifiedImage, dataObject.ModifiedImage, 100, 150);
+            }
+        }
+    }
+}
