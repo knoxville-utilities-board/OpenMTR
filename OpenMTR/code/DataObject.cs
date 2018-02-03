12227519ee5b9f1a7b92a1e22de91a4ed6f315c0
Bryan Dingman <bsdingman@gmail.com>
2018-02-02 22:38:49 -0500

    chore: Modified DataObject and dependancies (#20)

--- OpenMTR/code/DataObject.cs
+++ OpenMTR/code/DataObject.cs
@@ -5,14 +5,16 @@ namespace OpenMTR
     public class DataObject
     {
         public string FileName { get; set; }
-        public Mat Image { get; set; }
+        public Mat SourceImage { get; set; }
+        public Mat ModifiedImage { get; set; }
         public int MeterRead { get; set; }
 
         public DataObject() { }
-        public DataObject(string fileName, Mat image, int meterRead)
+        public DataObject(string fileName, Mat sourceImage, Mat modifiedImage, int meterRead)
         {
             this.FileName = fileName;
-            this.Image = image;
+            this.SourceImage = sourceImage;
+            this.ModifiedImage = modifiedImage;
             this.MeterRead = meterRead;
         }
     }
