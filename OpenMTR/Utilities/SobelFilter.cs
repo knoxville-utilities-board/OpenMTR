5d35d74df3514e58d63e44f268b6431b4271b8fa
Matt Clinard <cclinard84@gmail.com>
2018-02-02 22:39:57 -0500

    fix: Modified method parameters

--- OpenMTR/Utilities/SobelFilter.cs
+++ OpenMTR/Utilities/SobelFilter.cs
@@ -9,9 +9,9 @@ namespace OpenMTR
 {
     public static class SobelFilter
     {
-        public static void ApplySobelFilter(DataObject dataObject)
+        public static void ApplySobelFilter(Mat source, Mat destination)
         {
-            Cv2.Sobel(dataObject.ModifiedImage, dataObject.ModifiedImage, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
+            Cv2.Sobel(source, destination, MatType.CV_32F, xorder: 1, yorder: 0, ksize: -1);
         }
 
         public static void ApplySobelFilter(List<DataObject> dataObjectList)
