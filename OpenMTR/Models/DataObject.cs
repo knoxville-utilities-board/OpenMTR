94f2de19b65285a4c54212ab72be1feab884af8e
Matt <cclinard84@gmail.com>
2018-02-03 19:33:21 -0500

    fix: Moved DataObject class into separate models folder and renamed code Folder (#23)

---
+++ OpenMTR/Models/DataObject.cs
@@ -0,0 +1,21 @@
+﻿using OpenCvSharp;
+
+namespace OpenMTR
+{
+    public class DataObject
+    {
+        public string FileName { get; set; }
+        public Mat SourceImage { get; set; }
+        public Mat ModifiedImage { get; set; }
+        public int MeterRead { get; set; }
+
+        public DataObject() { }
+        public DataObject(string fileName, Mat sourceImage, Mat modifiedImage, int meterRead)
+        {
+            this.FileName = fileName;
+            this.SourceImage = sourceImage;
+            this.ModifiedImage = modifiedImage;
+            this.MeterRead = meterRead;
+        }
+    }
+}
