cfd56a586f7fffeb004464da954db9c5c7094582
xervir <nchietala@gmail.com>
2018-02-07 11:41:21 -0500

    chore: added GetKernel method to ImageUtils (#24)
    * chore: added GetKernel method to ImageUtils
    
    * fix: removed unnecessary Matrix reference
    
    * fix: removed hard coded kernel size
    
    * fix: required passing of size object to GetKernel

--- OpenMTR/Utilities/ImageUtils.cs
+++ OpenMTR/Utilities/ImageUtils.cs
@@ -34,5 +34,10 @@ namespace OpenMTR
                 Cv2.GaussianBlur(dataObject.SourceImage, dataObject.ModifiedImage, new Size(3, 3), 0, 0, BorderTypes.Default);
             }
         }
+
+        public static Mat GetKernel(Size size)
+        {
+            return Cv2.GetStructuringElement(MorphShapes.Rect, size);
+        }
     }
 }
