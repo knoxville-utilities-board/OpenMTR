de52b41800b890774e35b833442b69b6169e4079
Bryan Dingman <bsdingman@gmail.com>
2018-02-12 12:07:42 -0500

    feat: Abiltiy to draw debug lines for hough transformation checking to file

--- OpenMTR/Utilities/DebugUtils.cs
+++ OpenMTR/Utilities/DebugUtils.cs
@@ -29,5 +29,28 @@ namespace OpenMTR
         {
             return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
         }
+
+        public static void DrawLines(Mat sourceImage, List<LineSegmentPolar> lines)
+        {
+            foreach (LineSegmentPolar line in lines)
+            {
+                float rho = line.Rho;
+                float theta = line.Theta;
+
+                double a = Math.Cos(theta),
+                       b = Math.Sin(theta),
+                       x0 = a * rho,
+                       y0 = b * rho;
+
+                int x1 = (int)(x0 + 1000 * (-b)),
+                    y1 = (int)(y0 + 1000 * a),
+                    x2 = (int)(x0 - 1000 * (-b)),
+                    y2 = (int)(y0 - 1000 * (a));
+
+                Cv2.Line(sourceImage, new OpenCvSharp.Point(x1, y1), new OpenCvSharp.Point(x2, y2), new Scalar(255, 0, 0));
+            }
+
+            ExportMatToFile(sourceImage, "image_with_lines.jpg");
+        }
     }
 }
