cd30a75c1a25b16c9ce9d2fe7db83a9b93e0fd11
Bryan Dingman <bsdingman@gmail.com>
2018-02-12 12:20:06 -0500

    feat: Added methods for adjusting image skew

--- OpenMTR/Utilities/ImageUtils.cs
+++ OpenMTR/Utilities/ImageUtils.cs
@@ -39,5 +39,47 @@ namespace OpenMTR
         {
             return Cv2.GetStructuringElement(MorphShapes.Rect, size);
         }
+
+        public static void RotateImage(Mat sourceImage, Mat destinationImage, double degrees)
+        {
+            Mat rotationMatrix = Cv2.GetRotationMatrix2D(new Point2f(sourceImage.Cols / 2, sourceImage.Rows / 2), degrees, 1);
+            Cv2.WarpAffine(sourceImage, destinationImage, rotationMatrix, sourceImage.Size());
+        }
+
+        public static float DetectImageSkew(Mat sourceImage, Mat destinationImage)
+        {
+            List<LineSegmentPolar> filteredLines = new List<LineSegmentPolar>();
+            float theta_min = (float)(60f * Cv2.PI / 180f),
+                  theta_max = (float)(120f * Cv2.PI / 180f),
+                  theta_avr = 0f,
+                  theta_deg = 0f;
+
+            LineSegmentPolar[] lines = Cv2.HoughLines(destinationImage, 1, Cv2.PI / 180, 200);
+
+            foreach (LineSegmentPolar line in lines)
+            {
+                float theta = line.Theta;
+                if (theta >= theta_min && theta <= theta_max)
+                {
+                    filteredLines.Add(line);
+                    theta_avr += theta;
+                }
+            }
+
+            if (filteredLines.Count > 0)
+            {
+                theta_avr /= filteredLines.Count;
+                theta_deg = (float)(theta_avr / Cv2.PI * 180f) - 90;
+                DebugUtils.Log("Detected skew: " + theta_deg);
+            }
+            else
+            {
+                DebugUtils.Log("Failed to detect skew");
+            }
+
+            //DebugUtils.DrawLines(sourceImage, filteredLines);
+
+            return theta_deg;
+        }
     }
 }
