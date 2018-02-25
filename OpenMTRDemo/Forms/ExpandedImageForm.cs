4a22e5d6e9222db2325f1393380f923df7238f9a
nchietala <nchietala@gmail.com>
2018-02-25 12:31:27 -0500

    refactor: deleted ExpandedImageForm and renamed MainForm to take its place

--- OpenMTRDemo/Forms/ExpandedImageForm.cs
+++ OpenMTRDemo/Forms/ExpandedImageForm.cs
@@ -1,23 +1,113 @@
-﻿using OpenCvSharp;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
+using OpenCvSharp;
+using OpenMTRDemo.Forms;
 
 namespace OpenMTRDemo.Forms
 {
-    public partial class ExpandedImageForm : Form
+    public partial class ExpandedImageForm : BaseForm
     {
-        public ExpandedImageForm(Mat mat)
+        private Mat _source, _target;
+
+        public ExpandedImageForm(Mat image)
         {
+            _source = image;
+            _target = image.Clone();
             InitializeComponent();
-            pictureBoxExpanded.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBoxExpanded.Image = DemoUtilities.MatToBitmap(mat);   
+        }
+
+        private void Render()
+        {
+            InputImageBox.Image = DemoUtilities.MatToBitmap(_source);
+            RenderOutput();
+        }
+        private void RenderOutput()
+        {
+            _target = _source.Clone();
+            ApplyFilters(_target);
+            OutputImageBox.Image = DemoUtilities.MatToBitmap(_target);
+        }
+
+        private void ApplyFilters(Mat imageToFilter)
+        {
+            foreach (string filter in FilterListBox.SelectedItems)
+            {
+                switch (filter)
+                {
+                    case "Black and White":
+                        Cv2.CvtColor(imageToFilter, imageToFilter, ColorConversionCodes.BGR2GRAY);
+                        break;
+                    case "Gaussian Blur":
+                        Cv2.GaussianBlur(imageToFilter, imageToFilter, new Size(3, 3), 0, 0, BorderTypes.Default);
+                        break;
+                    case "Edge Finding":
+                        if (cannyRadio.Checked)
+                        {
+                            Cv2.Canny(imageToFilter.Clone(), imageToFilter, CannyThreshold1Slider.Value, CannyThreshold2Slider.Value);
+                        }
+                        else
+                        {
+                            
+                            Cv2.Sobel(imageToFilter, imageToFilter, MatType.CV_8U, xorder: 1, yorder: 0, ksize: -1);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void filterListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            edgeFindingBox.Enabled = FilterListBox.SelectedItems.Contains("Edge Finding");
+            RenderOutput();
+        }
+
+        public override void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetDisableableControls(false);
+            WidthTextBox.Text = "";
+            HeightTextBox.Text = "";
+            MetaDataTextBox.Text = "";
+            InputImageBox.Image = null;
+            OutputImageBox.Image = null;
+        }
+
+        private void SetDisableableControls(bool state)
+        {
+            SaveToolStripMenuItem.Enabled = state;
+            CloseToolStripMenuItem.Enabled = state;
+            FilterListBox.Enabled = state;
+            edgeFindingBox.Enabled = state && FilterListBox.SelectedItems.Contains("Edge Finding");
+            cannySettingsPanel.Enabled = cannyRadio.Checked;
+        }
+
+        private void cannyThreshold1Number_ValueChanged(object sender, EventArgs e)
+        {
+            CannyThreshold1Slider.Value = (int)CannyThreshold1Number.Value;
+            RenderOutput();
+        }
+
+        private void cannyThreshold2Number_ValueChanged(object sender, EventArgs e)
+        {
+            CannyThreshold2Slider.Value = (int)CannyThreshold2Number.Value;
+            RenderOutput();
+        }
+
+        private void cannyThreshold1Slider_Scroll(object sender, EventArgs e)
+        {
+            CannyThreshold1Number.Value = CannyThreshold1Slider.Value;
+            RenderOutput();
+        }
+
+        private void cannyThreshold2Slider_Scroll(object sender, EventArgs e)
+        {
+            CannyThreshold2Number.Value = CannyThreshold2Slider.Value;
+            RenderOutput();
+        }
+
+        private void cannyRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            SetDisableableControls(true);
+            RenderOutput();
         }
     }
 }
