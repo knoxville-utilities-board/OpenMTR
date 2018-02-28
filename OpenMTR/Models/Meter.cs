27d9017df47d4e4c0da723cbfde25f35c44db8f9
Bryan Dingman <bsdingman@gmail.com>
2018-02-28 11:20:53 -0500

    refactor: change meter to accept new json

--- OpenMTR/Models/Meter.cs
+++ OpenMTR/Models/Meter.cs
@@ -7,15 +7,21 @@ namespace OpenMTR
         public string FileName { get; set; }
         public Mat SourceImage { get; set; }
         public Mat ModifiedImage { get; set; }
-        public int MeterRead { get; set; }
+        public string MeterRead { get; set; }
+        public string ReadType { get; set; }
+        public string MeterType { get; set; }
+        public string Manufacturer { get; set; }
 
         public Meter() { }
-        public Meter(string fileName, Mat sourceImage, Mat modifiedImage, int meterRead)
+        public Meter(string fileName, Mat sourceImage, Mat modifiedImage, string meterRead, string readType, string meterType, string manufacturer)
         {
             this.FileName = fileName;
             this.SourceImage = sourceImage;
             this.ModifiedImage = modifiedImage;
             this.MeterRead = meterRead;
+            this.ReadType = readType;
+            this.MeterType = meterType;
+            this.Manufacturer = manufacturer;
         }
     }
 }
