44a6eb3700f05122615036ee8f1bfabdee6cf81e
Bryan Dingman <bsdingman@gmail.com>
2018-03-02 14:01:57 -0500

    chore: Add dial class

---
+++ OpenMTR/Code/Dial.cs
@@ -0,0 +1,17 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR
+{
+    public static class Dial
+    {
+        public static string Read(Meter meter, List<CircleSegment> dials)
+        {
+            return "";
+        }
+    }
+}
