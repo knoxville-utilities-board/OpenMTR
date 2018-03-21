efefe30a34df3c6d7a0bc945adc57d22fcaa8e92
Bryan Dingman <bsdingman@gmail.com>
2018-03-21 15:16:18 -0400

    fix: remove gaussian blur methods (#49)

--- OpenMTR/Utilities/ImageUtils.cs
+++ OpenMTR/Utilities/ImageUtils.cs
@@ -22,19 +22,6 @@ namespace OpenMTR
             }
         }
 
-        public static void ApplyGaussianBlur(Mat sourceImage, Mat destinationImage)
-        {
-            Cv2.GaussianBlur(sourceImage, destinationImage, new Size(5, 5), 0, 0, BorderTypes.Default);
-        }
-
-        public static void ApplyGaussianBlur(List<Meter> meterList)
-        {
-            foreach(Meter meter in meterList)
-            {
-                Cv2.GaussianBlur(meter.SourceImage, meter.ModifiedImage, new Size(3, 3), 0, 0, BorderTypes.Default);
-            }
-        }
-
         public static Mat GetKernel(Size size)
         {
             return Cv2.GetStructuringElement(MorphShapes.Rect, size);
