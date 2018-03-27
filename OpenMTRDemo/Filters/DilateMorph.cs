d98d6e2ea928985cf6f804cc10c959bc7b2eba6f
Matt <cclinard84@gmail.com>
2018-03-27 15:21:20 -0400

    feat: Added new morphological operations (#56)

---
+++ OpenMTRDemo/Filters/DilateMorph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenMTRDemo.Forms;
+using OpenMTRDemo.Models;
+using OpenCvSharp;
+
+namespace OpenMTRDemo.Filters
+{
+    public partial class DilateMorph : BaseFilter
+    {
+        public DilateMorph(ExpandedImageForm Editor = null, MeterImage Meter = null)
+        {
+            InitializeComponent();
+            this.Editor = Editor;
+            this.Meter = Meter;
+            FilterName = "Dilate";
+        }
+
+        public override void ApplyFilter(Mat image)
+        {
+            Cv2.Dilate(image, image, new Mat());
+        }
+
+        public override BaseFilter Clone()
+        {
+            return new DilateMorph(Editor, Meter);
+        }
+    }
+}
---
+++ OpenMTRDemo/Filters/MorphExOpening.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenMTRDemo.Forms;
+using OpenMTRDemo.Models;
+using OpenCvSharp;
+using OpenMTR;
+
+namespace OpenMTRDemo.Filters
+{
+    public partial class MorphExOpening : BaseFilter
+    {
+        public MorphExOpening(ExpandedImageForm Editor = null, MeterImage Meter = null)
+        {
+            InitializeComponent();
+            this.Editor = Editor;
+            this.Meter = Meter;
+            FilterName = "Morph Open";
+        }
+
+        public override void ApplyFilter(Mat image)
+        {
+            Cv2.MorphologyEx(image, image, MorphTypes.Open, ImageUtils.GetKernel(new OpenCvSharp.Size(3, 3)));
+        }
+
+        public override BaseFilter Clone()
+        {
+            return new MorphExClosing(Editor, Meter);
+        }
+    }
+}
