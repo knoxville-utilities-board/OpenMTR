efd7015e97483c63d696a20123bbb3bc2fbe7fd5
Bryan Dingman <bsdingman@gmail.com>
2018-04-04 10:55:31 -0400

    fix: Dial read being out of bounds

--- OpenMTR/Code/Dial.cs
+++ OpenMTR/Code/Dial.cs
@@ -48,39 +48,22 @@ namespace OpenMTR
             for (int i = 0; i < circles.Count; i++)
             {
                 CircleSegment circle = circles[i];
-                Mat dial = new Mat(meter.SourceImage, new Rect((int)(circle.Center.X - circle.Radius), (int)(circle.Center.Y - circle.Radius), (int)(circle.Radius * 2 + 1), (int)(circle.Radius * 2 + 1)));
+                int rectX = (int)(circle.Center.X - circle.Radius),
+                    rectY = (int)(circle.Center.Y - circle.Radius),
+                    rectW = (int)(circle.Radius * 2 + 1),
+                    rectH = (int)(circle.Radius * 2 + 1);
+                if ((rectX + rectW) > meter.SourceImage.Width)
+                {
+                    rectW = (rectX + rectW) - meter.SourceImage.Width;
+                }
+                if ((rectY + rectH) > meter.SourceImage.Height)
+                {
+                    rectW = (rectY + rectH) - meter.SourceImage.Height;
+                }
+                Mat dial = new Mat(meter.SourceImage, new Rect(rectX, rectY, rectW, rectH));
                 Point center = new Point(dial.Width / 2, dial.Height / 2);
                 Point needleTip = DetectNeedleTip(dial, center);
-
-                Dictionary<char, List<Point>> clockwise = new Dictionary<char, List<Point>>()
-                {
-                    { '0', new List<Point>() { new Point(center.X * 0.30, 0), new Point(center.X + (center.X * 0.70), 0) } },
-                    { '1', new List<Point>() { new Point(center.X + (center.X * 0.70), 0), new Point(dial.Width, center.Y * 0.65) } },
-                    { '2', new List<Point>() { new Point(dial.Width, center.Y * 0.65), new Point(dial.Width, center.Y + (center.Y * 0.30)) } },
-                    { '3', new List<Point>() { new Point(dial.Width, center.Y + (center.Y * 0.30)), new Point(center.X + (center.X * 0.65), dial.Height) } },
-                    { '4', new List<Point>() { new Point(center.X + (center.X * 0.65), dial.Height), new Point(center.X, dial.Width) } },
-                    { '5', new List<Point>() { new Point(center.X, dial.Width), new Point(center.X * 0.20, dial.Height) } },
-                    { '6', new List<Point>() { new Point(center.X * 0.20, dial.Height), new Point(0, center.Y + (center.Y * 0.30)) } },
-                    { '7', new List<Point>() { new Point(0, center.Y + (center.Y * 0.30)), new Point(0, center.Y * 0.65) } },
-                    { '8', new List<Point>() { new Point(0, center.Y * 0.65), new Point(center.X * 0.30, 0) } },
-                    { '9', new List<Point>() { new Point(center.X * 0.30, 0), new Point(center.X, 0) } },
-                };
-
-                Dictionary<char, List<Point>> counterClockwise = new Dictionary<char, List<Point>>()
-                {
-                    { '0', new List<Point>() { new Point(center.X, 0), new Point(center.X * 0.30, 0) } },
-                    { '1', new List<Point>() { new Point(center.X * 0.30, 0), new Point(0, center.Y * 0.65) } },
-                    { '2', new List<Point>() { new Point(0, center.Y * 0.65), new Point(0, center.Y + (center.Y * 0.30)) } },
-                    { '3', new List<Point>() { new Point(0, center.Y + (center.Y * 0.30)), new Point(center.X * 0.20, dial.Height) } },
-                    { '4', new List<Point>() { new Point(center.X * 0.20, dial.Height), new Point(center.X, dial.Width) } },
-                    { '5', new List<Point>() { new Point(center.X, dial.Width), new Point(center.X + (center.X * 0.65), dial.Height) } },
-                    { '6', new List<Point>() { new Point(center.X + (center.X * 0.65), dial.Height), new Point(dial.Width, center.Y + (center.Y * 0.30)) } },
-                    { '7', new List<Point>() { new Point(dial.Width, center.Y + (center.Y * 0.30)), new Point(dial.Width, center.Y * 0.65) } },
-                    { '8', new List<Point>() { new Point(dial.Width, center.Y * 0.65), new Point(center.X + (center.X * 0.70), 0) } },
-                    { '9', new List<Point>() { new Point(center.X + (center.X * 0.70), 0), new Point(center.X, 0) } }
-                };
-
-                foreach (KeyValuePair<char, List<Point>> numberPosition in (MathUtils.IsEven(i)) ? counterClockwise : clockwise)
+                foreach (KeyValuePair<char, List<Point>> numberPosition in (MathUtils.IsEven(i)) ? CreateCounterClockwiseSegments(dial, center) : CreateClockwiseSegments(dial, center))
                 {
                     if (MathUtils.IsPointInTriangle(needleTip, center, numberPosition.Value[0], numberPosition.Value[1]))
                     {
@@ -89,6 +72,40 @@ namespace OpenMTR
                 }
             }
             return readValue.ToString();
+        }
+
+        private static Dictionary<char, List<Point>> CreateClockwiseSegments(Mat dial, Point center)
+        {
+            return new Dictionary<char, List<Point>>()
+            {
+                { '0', new List<Point>() { new Point(center.X * 0.30, 0), new Point(center.X + (center.X * 0.70), 0) } },
+                { '1', new List<Point>() { new Point(center.X + (center.X * 0.70), 0), new Point(dial.Width, center.Y * 0.65) } },
+                { '2', new List<Point>() { new Point(dial.Width, center.Y * 0.65), new Point(dial.Width, center.Y + (center.Y * 0.30)) } },
+                { '3', new List<Point>() { new Point(dial.Width, center.Y + (center.Y * 0.30)), new Point(center.X + (center.X * 0.65), dial.Height) } },
+                { '4', new List<Point>() { new Point(center.X + (center.X * 0.65), dial.Height), new Point(center.X, dial.Width) } },
+                { '5', new List<Point>() { new Point(center.X, dial.Width), new Point(center.X * 0.20, dial.Height) } },
+                { '6', new List<Point>() { new Point(center.X * 0.20, dial.Height), new Point(0, center.Y + (center.Y * 0.30)) } },
+                { '7', new List<Point>() { new Point(0, center.Y + (center.Y * 0.30)), new Point(0, center.Y * 0.65) } },
+                { '8', new List<Point>() { new Point(0, center.Y * 0.65), new Point(center.X * 0.30, 0) } },
+                { '9', new List<Point>() { new Point(center.X * 0.30, 0), new Point(center.X, 0) } },
+            };
+        }
+
+        private static Dictionary<char, List<Point>> CreateCounterClockwiseSegments(Mat dial, Point center)
+        {
+            return new Dictionary<char, List<Point>>()
+            {
+                { '0', new List<Point>() { new Point(center.X, 0), new Point(center.X * 0.30, 0) } },
+                { '1', new List<Point>() { new Point(center.X * 0.30, 0), new Point(0, center.Y * 0.65) } },
+                { '2', new List<Point>() { new Point(0, center.Y * 0.65), new Point(0, center.Y + (center.Y * 0.30)) } },
+                { '3', new List<Point>() { new Point(0, center.Y + (center.Y * 0.30)), new Point(center.X * 0.20, dial.Height) } },
+                { '4', new List<Point>() { new Point(center.X * 0.20, dial.Height), new Point(center.X, dial.Width) } },
+                { '5', new List<Point>() { new Point(center.X, dial.Width), new Point(center.X + (center.X * 0.65), dial.Height) } },
+                { '6', new List<Point>() { new Point(center.X + (center.X * 0.65), dial.Height), new Point(dial.Width, center.Y + (center.Y * 0.30)) } },
+                { '7', new List<Point>() { new Point(dial.Width, center.Y + (center.Y * 0.30)), new Point(dial.Width, center.Y * 0.65) } },
+                { '8', new List<Point>() { new Point(dial.Width, center.Y * 0.65), new Point(center.X + (center.X * 0.70), 0) } },
+                { '9', new List<Point>() { new Point(center.X + (center.X * 0.70), 0), new Point(center.X, 0) } }
+            };
         }
     }
 }
