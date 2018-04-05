9920fb04452c6eeca9a7a434a849f76653c2f847
Bryan Dingman <bsdingman@gmail.com>
2018-04-04 20:10:42 -0400

    fix: naming convention and removed comments from ProgessBar

--- OpenMTR/Models/ProgressBar.cs
+++ OpenMTR/Models/ProgressBar.cs
@@ -11,26 +11,20 @@ namespace OpenMTR
     /// </summary>
     public class ProgressBar : IDisposable, IProgress<double>
     {
-        private const int blockCount = 10;
-        private readonly TimeSpan animationInterval = TimeSpan.FromSeconds(1.0 / 8);
-        private const string animation = @"|/-\";
-
-        private readonly Timer timer;
-
-        private double currentProgress = 0;
-        private string currentText = string.Empty;
-        private bool disposed = false;
-        private int animationIndex = 0;
+        private const int _blockCount = 10;
+        private readonly TimeSpan _animationInterval = TimeSpan.FromSeconds(1.0 / 8);
+        private const string _animation = @"|/-\";
+        private readonly Timer _timer;
+        private double _currentProgress = 0;
+        private string _currentText = string.Empty;
+        private bool _disposed = false;
+        private int _animationIndex = 0;
         private string _title = "";
 
         public ProgressBar(string title = "")
         {
-            timer = new Timer(TimerHandler);
+            _timer = new Timer(TimerHandler);
             _title = title;
-
-            // A progress bar is only for temporary display in a console window.
-            // If the console output is redirected to a file, draw nothing.
-            // Otherwise, we'll end up with a lot of garbage in the target file.
             if (!Console.IsOutputRedirected)
             {
                 ResetTimer();
@@ -39,24 +33,23 @@ namespace OpenMTR
 
         public void Report(double value)
         {
-            // Make sure value is in [0..1] range
             value = Math.Max(0, Math.Min(1, value));
-            Interlocked.Exchange(ref currentProgress, value);
+            Interlocked.Exchange(ref _currentProgress, value);
         }
 
         private void TimerHandler(object state)
         {
-            lock (timer)
+            lock (_timer)
             {
-                if (disposed) return;
+                if (_disposed) return;
 
-                int progressBlockCount = (int)(currentProgress * blockCount);
-                int percent = (int)(currentProgress * 100);
+                int progressBlockCount = (int)(_currentProgress * _blockCount);
+                int percent = (int)(_currentProgress * 100);
                 string text = string.Format("{0} [{1}{2}] {3,4}% {4}",
                     _title,
-                    new string('#', progressBlockCount), new string('-', blockCount - progressBlockCount),
+                    new string('#', progressBlockCount), new string('-', _blockCount - progressBlockCount),
                     percent,
-                    animation[animationIndex++ % animation.Length]);
+                    _animation[_animationIndex++ % _animation.Length]);
                 UpdateText(text);
 
                 ResetTimer();
@@ -65,23 +58,16 @@ namespace OpenMTR
 
         private void UpdateText(string text)
         {
-            // Get length of common portion
             int commonPrefixLength = 0;
-            int commonLength = Math.Min(currentText.Length, text.Length);
-            while (commonPrefixLength < commonLength && text[commonPrefixLength] == currentText[commonPrefixLength])
+            int commonLength = Math.Min(_currentText.Length, text.Length);
+            while (commonPrefixLength < commonLength && text[commonPrefixLength] == _currentText[commonPrefixLength])
             {
                 commonPrefixLength++;
             }
-
-            // Backtrack to the first differing character
             StringBuilder outputBuilder = new StringBuilder();
-            outputBuilder.Append('\b', currentText.Length - commonPrefixLength);
-
-            // Output new suffix
+            outputBuilder.Append('\b', _currentText.Length - commonPrefixLength);
             outputBuilder.Append(text.Substring(commonPrefixLength));
-
-            // If the new text is shorter than the old one: delete overlapping characters
-            int overlapCount = currentText.Length - text.Length;
+            int overlapCount = _currentText.Length - text.Length;
             if (overlapCount > 0)
             {
                 outputBuilder.Append(' ', overlapCount);
@@ -89,19 +75,19 @@ namespace OpenMTR
             }
 
             Console.Write(outputBuilder);
-            currentText = text;
+            _currentText = text;
         }
 
         private void ResetTimer()
         {
-            timer.Change(animationInterval, TimeSpan.FromMilliseconds(-1));
+            _timer.Change(_animationInterval, TimeSpan.FromMilliseconds(-1));
         }
 
         public void Dispose()
         {
-            lock (timer)
+            lock (_timer)
             {
-                disposed = true;
+                _disposed = true;
                 UpdateText(string.Empty);
             }
         }
