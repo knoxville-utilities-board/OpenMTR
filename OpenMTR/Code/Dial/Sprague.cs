cf4e60dd720eec073deb6c8889231e795182bf3c
Bryan Dingman <bsdingman@gmail.com>
2018-04-07 20:16:27 -0400

    feat: Improved Sprague detection

---
+++ OpenMTR/Code/Dial/Sprague.cs
@@ -0,0 +1,68 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMTR
+{
+    public static class Sprague
+    {
+        public static void ReadMeter(Meter meter)
+        {
+            try
+            {
+                if (ExtractionFirstPass(meter))
+                {
+                    throw new SuccessfulReadException();
+                }
+
+                throw new FailReadException();
+            }
+            catch (SuccessfulReadException)
+            {
+                Report.AddSuccessfulRead(meter);
+            }
+            catch (FailReadException)
+            {
+                Report.AddFailedRead(meter);
+            }
+        }
+
+        private static bool ExtractionFirstPass(Meter meter)
+        {
+            List<CircleSegment> filteredCircles = new List<CircleSegment>();
+            ImageUtils.AdjustImageSkew(meter);
+            ImageUtils.ColorToGray(meter.SourceImage, meter.ModifiedImage);
+            CircleSegment[] circles = Cv2.HoughCircles(meter.ModifiedImage, HoughMethods.Gradient, 1, meter.ModifiedImage.Rows / 20, 250);
+            for (int i = 0; i < circles.Length; i++)
+            {
+                CircleSegment circle = circles[i];
+                Point center = circle.Center;
+                int count = 0;
+                foreach (CircleSegment otherCircle in circles)
+                {
+                    if (!circle.Equals(otherCircle))
+                    {
+                        if (Math.Abs(center.Y - otherCircle.Center.Y) < 10)
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count == 3)
+                {
+                    filteredCircles.Add(circle);
+                }
+            }
+
+            if (filteredCircles.Count != 4)
+            {
+                return false;
+            }
+
+            return Dial.Read(meter, filteredCircles) == meter.MetaData.MeterRead;
+        }
+    }
+}
