8af676464b98a5ba4e29b41a27efcf43ae1ec787
Nick Hietala <nchietala@gmail.com>
2018-04-11 15:02:46 -0400

    feat: added trackbars to morph open and close (#64)
    * feat: added trackbars to morph open and close
    
    * feat: added bars to dilate
    
    * fix: removed csproj reference to missing file

--- OpenMTRDemo/Filters/MorphExClosing.cs
+++ OpenMTRDemo/Filters/MorphExClosing.cs
@@ -16,22 +16,29 @@ namespace OpenMTRDemo.Filters
 {
     public partial class MorphExClosing : BaseFilter
     {
-        public MorphExClosing(ExpandedImageForm Editor = null, MeterImage Meter = null)
+        public MorphExClosing(ExpandedImageForm Editor = null, MeterImage Meter = null, int hSize = 1, int vSize = 1)
         {
             InitializeComponent();
             this.Editor = Editor;
             this.Meter = Meter;
             FilterName = "Morph Close";
+            horizontalTrackBar.Value = hSize;
+            verticalTrackBar.Value = vSize;
         }
 
         public override void ApplyFilter(Mat image)
         {
-            Cv2.MorphologyEx(image, image, MorphTypes.Close, ImageUtils.GetKernel(new OpenCvSharp.Size(3, 3)));
+            Cv2.MorphologyEx(image, image, MorphTypes.Close, ImageUtils.GetKernel(new OpenCvSharp.Size(1 + 2 * horizontalTrackBar.Value, 1 + 2 * verticalTrackBar.Value)));
         }
 
         public override BaseFilter Clone()
         {
-            return new MorphExClosing(Editor, Meter);
+            return new MorphExClosing(Editor, Meter, horizontalTrackBar.Value, verticalTrackBar.Value);
+        }
+
+        private void Render(object sender, EventArgs e)
+        {
+            Render();
         }
     }
 }
